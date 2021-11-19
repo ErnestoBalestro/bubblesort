75fdbfcf402272af0d4b61f37d0a914b0d1e4974
Ernesto Balestro <ernestobalestro@outlook.com>
2021-11-19 13:27:41 -0300

    Mockito_Ernesto

---
+++ src/test/java/edu/ifrs/vvs/import static org.mockito.Mockito.*;.java
@@ -0,0 +1,23 @@
+import static org.mockito.Mockito.*;
+
+// mock creation
+List mockedList = mock(List.class);
+
+// using mock object - it does not throw any "unexpected interaction" exception
+mockedList.add("one");
+mockedList.clear();
+
+// selective, explicit, highly readable verification
+verify(mockedList).add("one");
+verify(mockedList).clear();
+// you can mock concrete classes, not only interfaces
+LinkedList mockedList = mock(LinkedList.class);
+
+// stubbing appears before the actual execution
+when(mockedList.get(0)).thenReturn("first");
+
+// the following prints "first"
+System.out.println(mockedList.get(0));
+
+// the following prints "null" because get(999) was not stubbed
+System.out.println(mockedList.get(999));
